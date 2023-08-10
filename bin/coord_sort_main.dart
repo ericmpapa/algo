de362a16320693d2a9f1b9647e393e1526fe218e
Eric Mpapa <17198942+ericmpapa@users.noreply.github.com>
2023-08-10 19:04:19 +0200

    update

--- bin/coord_sort_main.dart
+++ bin/coord_sort_main.dart
@@ -4,20 +4,16 @@ import 'package:algorithms/stdout.dart' as stdout;
 import 'package:algorithms/stdin.dart' as stdin;
 
 void main() {
-  stdout.println("Execution de l'algorithme de Tri par séléction des coordonnées");
-  stdout.println("--------------------------------------------------------------");
-  stdout.println("Lecture des inputs");
-  Stopwatch timer = Stopwatch();
-  timer.start();
+  
+ 
   var inputs = stdin.readCoords();
   /*print("tableau non trié");
   print("----------------");
   print(inputs);*/
   var origin = Coord(1.5609,30.2551);
-  selection_sort.sortCoord(inputs,origin);
-  timer.stop();
-  stdout.println("temps écoulé: ${timer.elapsed}");
-  ("tableau trié");
-  print("------------");
-  print(inputs);
+  //selection_sort.sortCoord(inputs,origin);
+  
+  for(int i=0;i<inputs.length;i++){
+    print(double.parse(inputs[i].distance(origin).toStringAsFixed(4)));
+  }
 }
